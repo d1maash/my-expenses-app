bd33efdf30157644d9b84a622866a2bbe0f4359f
Dimash <dimashaijarikov@gmail.com>
2024-08-04 12:27:31 +0500

    Initial Commit

---
+++ my-expenses-app/my_expenses_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_expenses_appApp.swift
+//  my-expenses-app
+//
+//  Created by Динмуханбет Айжарыков on 04.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct my_expenses_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
